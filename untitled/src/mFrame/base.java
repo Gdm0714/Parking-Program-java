701cd333a7b7427745c907b7dbe5ba468496fdaf
dongmin <ehdals45454@naver.com>
2022-11-27 21:48:53 +0900

    11/27 9:48

--- untitled/src/mFrame/base.java
+++ untitled/src/mFrame/base.java
@@ -8,43 +8,40 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class base extends JFrame {
-    private JTextField id = new JTextField("id 입력");
     private JTextField pw = new JTextField("pw 입력");
     private JButton log = new JButton("로그인");
-    private JButton jb = new JButton("회원가입");
     private JLabel pi;
-    private ImageIcon parking = new ImageIcon("images/parkingicon.png");
+    private ImageIcon parking = new ImageIcon("C:\\Users\\고동민\\Desktop\\자바팀플 20192601 고동민\\Parking-Program-java\\untitled\\images\\logo2.png");
     DB_Connection db = new DB_Connection();
 
     public base() {
         db.connect();
-        setTitle("메인 화면");
+        setTitle("로그인 화면");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Container c = getContentPane();
         c.setLayout(null);
-        id.setBounds(220, 300, 160, 30);
         pw.setBounds(220, 330, 160, 30);
-        c.add(id);
         c.add(pw);
-        pi = new JLabel(parking);
-        pi.setBounds(200, 100, 200, 200);
-        c.add(pi);
+        //pi = new JLabel(parking);
+        //pi.setBounds(200, 100, 300, 300);
+        //c.add(pi);
+
+        pw.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                pw.setText("");
+            }
+        });
         log.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent e) {
-                if(db.login(id.getText(), pw.getText())){
+                if(db.login(pw.getText())){
                     new Parking();
+                    setVisible(false);
                 };
             }
         });
-        jb.addMouseListener(new MouseAdapter() {
-            public void mouseClicked(MouseEvent e) {
-                new sign_Dialog();
-            }
-        });
         log.setBounds(200, 360, 100, 50);
-        jb.setBounds(300, 360, 100, 50);
         c.add(log);
-        c.add(jb);
         setSize(600, 600);
         setVisible(true);
     }
