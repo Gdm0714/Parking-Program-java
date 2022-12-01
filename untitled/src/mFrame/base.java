4410c71b82f14efd1e79f814cddb53721f8867db
kjm9547 <50660458+kjm9547@users.noreply.github.com>
2022-12-02 02:03:42 +0900

    Update base.java
    변경 사항++
    스레드 추가 
    버튼 추가
    예정++
    투명화 지정 
    배경 지정
    예외 처리

--- untitled/src/mFrame/base.java
+++ untitled/src/mFrame/base.java
@@ -7,11 +7,17 @@ import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.Arc2D;
 
 public class base extends JFrame {
+    //컴포넌트 전역 변수 선언
+    Container c;
     private JPanel header = new JPanel();
     private Line content = new Line();
     private JPanel footer = new JPanel();
@@ -21,24 +27,35 @@ public class base extends JFrame {
     private JButton log = new JButton("로그인");
     private JLabel pi;
     private ImageIcon parking = new ImageIcon("images/parkingicon.png");
+    private JLabel Password_t = new JLabel("Password");
+    //
+
     JComboBox <String> lc_box = new JComboBox<String>();
     String location[] = {"장영실관","하현관","늘빛관"};
     String db_name[]={"car_info","car_info_b","car_info_c"};
     DB_Connection db = new DB_Connection();
-
+    CircleAnimation circle;
     public String get_table_name(){
         return "";
     }
+    boolean check_info = false;
+    int arc; // 각도
+
+    JButton enter_bt = new JButton("접속");
+
+
+    //스타일 지정 변수
+    Font title_font = new Font("배민도현체",Font.BOLD,22);
     public base() {
 
-       db.connect();
+        db.connect();
         setTitle("메인 화면");
-       for(int i =0;  i<3;i++){
-           lc_box.addItem(location[i]);
-       }
+        for(int i =0;  i<3;i++){
+            lc_box.addItem(location[i]);
+        }
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Container c = getContentPane();
+        c =getContentPane();
         c.setLayout(new GridBagLayout());
         c.setBackground(Color.white);
         GridBagConstraints gbc = new GridBagConstraints();
@@ -48,17 +65,19 @@ public class base extends JFrame {
 
 
 
-//
+
 
         pi = new JLabel(parking);
         log.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent e) {
-               if(db.login(lc_box.getSelectedItem().toString(), pw.getText())==true){
-                   System.out.println(lc_box.getSelectedIndex());
-                   System.out.println("eqweqweqweqweqweqw");
-                   new Parking(lc_box.getSelectedItem().toString(),db_name[lc_box.getSelectedIndex()]);
-
-               };
+                pw.setVisible(false);
+                lc_box.setVisible(false);
+                log.setVisible(false);
+                Password_t.setVisible(false);
+                circle = new CircleAnimation();
+                circle.setBounds(0,166,600,333);
+                content.add(circle);
+                circle.setVisible(true);
 
             }
         });
@@ -76,8 +95,8 @@ public class base extends JFrame {
 
         header.setLayout(null);
         JLabel title = new JLabel("  Welcome The Parking System.");
-        title.setBounds(100,c.getHeight()/10,600,100);
-        Font title_font = new Font("배민도현체",Font.BOLD,25);
+        title.setBounds(130,c.getHeight()/10,600,100);
+
         title.setFont(title_font);
 
         header.add(title);
@@ -89,13 +108,13 @@ public class base extends JFrame {
         gbc.gridheight =3;
         c.add(content,gbc);
         content.setBackground(Color.WHITE);
-
+    //content.setBorder(new LineBorder(Color.red,5));
         content.setLayout(null);
-        JLabel content_text = new JLabel("Hello SuperVisor");
-        content_text.setBounds(180,c.getHeight()/8,600,100);
+        JLabel content_text = new JLabel("카놀라유");
+        content_text.setBounds(250,c.getHeight()/8,600,100);
         content_text.setFont(title_font);
         content.add(content_text);
-        JLabel Password_t = new JLabel("Password");
+
         Password_t.setBounds(180,c.getHeight()/8+90,200,50);
         //반투명 설정?Color test = new Color(0,0,0,122);
         Password_t.setFont(new Font("고딕",Font.PLAIN,10));
@@ -108,19 +127,35 @@ public class base extends JFrame {
         content.add(lc_box);
 
 
+
+
+
         gbc.weighty = 0.25;
         gbc.gridx = 0;
         gbc.gridy = 5;
         gbc.gridwidth =1;
         gbc.gridheight =2;
         c.add(footer,gbc);
-       footer.setBackground(Color.WHITE);
+        footer.setBackground(Color.WHITE);
 
         footer.setLayout(null);
         log.setBounds(170,c.getHeight()/6,200,50);
         log.setForeground(Color.white);
         log.setBackground(Color.black);
         footer.add(log);
+
+        enter_bt.setBounds(190,c.getHeight()/6,200,50);
+        enter_bt.setForeground(Color.white);
+        enter_bt.setBackground(Color.black);
+        footer.add(enter_bt);
+        enter_bt.setVisible(false);
+        enter_bt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new Parking(lc_box.getSelectedItem().toString(),db_name[lc_box.getSelectedIndex()]);
+            }
+        });
+
         setSize(600, 600);
         setVisible(true);
     }
@@ -133,6 +168,52 @@ public class base extends JFrame {
 
         }
     }
+    public class CircleAnimation extends JPanel {
+        public CircleAnimation(){
+            JLabel hello_msg =new JLabel("Hello SuperVisor");
+           // hello_msg.setBorder(new TitledBorder(new LineBorder(Color.BLUE,5)));
+            hello_msg.setFont(title_font);
+            check_info = db.login(lc_box.getSelectedItem().toString(), pw.getText());
+            new Thread(() -> {
+
+                while (arc <= 360) {
+                    arc++;
+                    repaint();
+                    revalidate();
+                    if(arc==360 && check_info==true){
+                        enter_bt.setVisible(true);
+                        setVisible(false);
+                        hello_msg.setBounds(210,c.getHeight()/8+125,200,50);
+                        content.add(hello_msg);
+                        hello_msg.setVisible(true);
+                    }
+                    try {
+                        Thread.sleep(10);
+
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        }
+
+                public void paintComponent(Graphics g) {
+                    super.paintComponent(g);
+                    setBackground(Color.white);
+                    Graphics2D g2d = (Graphics2D) g;
+                    g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                    g2d.setColor(new Color(121,140,53));
+                    g2d.fillOval(220, 0, 100, 100);
+                    g2d.setColor(new Color(242,189,29));
+                    g2d.fill(new Arc2D.Float(220, 0,100, 100, 90, arc, Arc2D.PIE));
+                    g2d.setColor(Color.white);
+
+                    g2d.fillOval(240, 20, 60, 60);
+                }
+
+        }
+
     public static void main(String[] args) {
         new base();
     }
